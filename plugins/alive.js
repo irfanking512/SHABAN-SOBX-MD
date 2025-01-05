4342bead071869d135e7923516ee31058c70575f
IRFAN WAHGA <127504180+irfanking512@users.noreply.github.com>
2025-01-05 21:54:15 +0500

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -13,16 +13,16 @@ cmd({
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
         // Generate system status message
-        const status = `╭━━〔 *SHABAN-SOBX-MD* 〕━━┈⊷
+        const status = `╭━━〔 *𝐈𝐑𝐅𝐀𝐍 𝐖𝐀𝐇𝐆𝐀* 〕━━┈⊷
 ┃◈╭─────────────·๏
 ┃◈┃• *⏳Uptime*:  ${runtime(process.uptime())} 
 ┃◈┃• *📟 Ram usage*: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${(os.totalmem() / 1024 / 1024).toFixed(2)}MB
 ┃◈┃• *⚙️ HostName*: ${os.hostname()}
-┃◈┃• *👨‍💻 Owner*: Mʀ Sʜᴀʙᴀɴ
+┃◈┃• *👨‍💻 Owner*: 𝐈𝐑𝐅𝐀𝐍 𝐖𝐀𝐇𝐆𝐀
 ┃◈┃• *🧬 Version*: 3.0.0 BETA
 ┃◈└───────────┈⊷
 ╰──────────────┈⊷
-> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ Mʀ Sʜᴀʙᴀɴ`;
+> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𝐈𝐑𝐅𝐀𝐍 𝐖𝐀𝐇𝐆𝐀`;
 
         // Send the status message with an image
         await conn.sendMessage(from, { 
