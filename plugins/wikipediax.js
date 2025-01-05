38798715c500a083d0d6b11b2f4915a7de4bf02c
IRFAN WAHGA <127504180+irfanking512@users.noreply.github.com>
2025-01-05 22:28:13 +0500

    Update wikipediax.js

--- plugins/wikipediax.js
+++ plugins/wikipediax.js
@@ -31,7 +31,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
 🔗 *URL*: ${summary.content_urls.desktop.page}
 
-> @ Pᴏᴡᴇʀᴇᴅ Bʏ Mʀ Sʜᴀʙᴀɴ `;
+> @ Pᴏᴡᴇʀᴇᴅ Bʏ 𝐈𝐑𝐅𝐀𝐍 𝐖𝐀𝐇𝐆𝐀 `;
 
         // Send the reply with the thumbnail image
         await conn.sendMessage(from, { image: { url: summary.originalimage.source }, caption: replyText }, { quoted: mek });
